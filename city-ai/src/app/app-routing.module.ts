f4c94997d11344ff910c1c1a7e23118f4e9a402d
parmar-abhinav <abhinav.parmar@infosys.com>
2024-09-23 17:30:10 +0530

    Added different component

--- city-ai/src/app/app-routing.module.ts
+++ city-ai/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReportissueComponent } from './reportissue/reportissue.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, // Default route
-  { path: 'about', component: AboutComponent },
-  { path: 'contact', component: ContactComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'issues', component: ReportissueComponent },
   { path: '**', redirectTo: '' } // Redirects any unknown route to home
 ];
 
